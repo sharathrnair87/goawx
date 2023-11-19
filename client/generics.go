c5f9a5123c4a45de5d2911607b6b4d3f1b6f3395
Sharath <sharathrnair87@gmail.com>
2023-11-20 09:54:44 +1100

    Test Generic GetAllPages

---
+++ client/generics.go
@@ -0,0 +1,57 @@
+package awx
+
+import (
+	//"bytes"
+	//"encoding/json"
+	//"fmt"
+	"net/url"
+)
+
+type ListResourceResponse[T any] struct {
+	Pagination
+	Results []*T `json:"results"`
+}
+
+type ResourceService struct {
+	client *Client
+}
+
+func GetAllPages[T any](client *Client, firstURL string, params map[string]string) ([]*T, error) {
+	results := make([]*T, 0)
+	nextURL := firstURL
+	for {
+		nextURLParsed, err := url.Parse(nextURL)
+		if err != nil {
+			return nil, err
+		}
+
+		nextURLQueryParams := make(map[string]string)
+		for paramName, paramValues := range nextURLParsed.Query() {
+			if len(paramValues) > 0 {
+				nextURLQueryParams[paramName] = paramValues[0]
+			}
+		}
+
+		for paramName, paramValue := range params {
+			nextURLQueryParams[paramName] = paramValue
+		}
+
+		result := new(ListResourceResponse[T])
+		resp, err := client.Requester.GetJSON(nextURLParsed.Path, result, nextURLQueryParams)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := CheckResponse(resp); err != nil {
+			return nil, err
+		}
+
+		results = append(results, result.Results...)
+
+		if result.Next == nil || result.Next.(string) == "" {
+			break
+		}
+		nextURL = result.Next.(string)
+	}
+	return results, nil
+}
